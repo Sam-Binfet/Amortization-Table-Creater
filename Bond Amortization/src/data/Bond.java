5bd9c07898350682d4cf6a45b529879e671c9a82
Sam Binfet <sbinf370@mtroyal.ca>
2019-08-12 16:54:20 -0600

    fixed file issues

--- Bond Amortization/src/data/Bond.java
+++ Bond Amortization/src/data/Bond.java
@@ -1,6 +1,9 @@
-package data;
+package src.data;
 
 public class Bond {
+
+
+
 	
 	private String client;
 	private double issueAmount;
@@ -9,41 +12,78 @@ public class Bond {
 	private double couponRate;
 	private double marketRate;
 	private int installementPeriodMonths;
-	private int payment;
+	private double payment;
 	private boolean premium;
 	private int numPayments;
 	private double adjustedIssueAmount;
+	private double adjustedMarketRate;
+	private double adjustedCouponRate;
 	
 	
 	public Bond(String client, double issueAmount, int termLengthYears, 
 				int termLengthMonths, double couponRate, double marketRate,
-				int installmentPeriodMonths) {
+				int installementPeriodMonths) {
+		this.client = client;
+		this.adjustedIssueAmount = issueAmount;
+		this.termLengthYears = termLengthYears;
+		this.termLengthMonths = termLengthMonths;
+		this.couponRate = couponRate;
+		this.marketRate = marketRate;
+		this.installementPeriodMonths = installementPeriodMonths;
+		calcAdjustedCouponRate();
+		calcAdjustedMarketRate();
+		calcNumPayments();
+		calcAdjustedIssueAmount();
+		
+		calcPayment();
+		
+		
 		
 	}
 
 
 	public boolean isPremium(double couponRate, double marketRate) {
-		// TODO Auto-generated method stub
-		return false;
+		return couponRate > marketRate;
 	}
 
 
-	public double calcAdjustedIssueAmount(double issueAmount, double couponRate, double marketRate,
-											double payment, int numPayments) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void calcAdjustedIssueAmount() {
+		
+		adjustedIssueAmount = issueAmount / Math.pow(1 + adjustedMarketRate, numPayments) + payment *
+								(1 - (Math.pow(1 + adjustedMarketRate, (numPayments * -1))) / adjustedMarketRate);
 	}
 
 
-	public double calcPayment(double issueAmount2, double couponRate2, int installementPeriodMonths2) {
-		// TODO Auto-generated method stub
-		return 0;
+	private void calcPayment() {
+		payment = issueAmount * adjustedCouponRate;
 	}
 
 
-	public int calcNumPayments(int termLengthYears2, int termLengthMonths2, int installementPeriodMonths2) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void calcNumPayments() {
+		int totalMonths = termLengthYears * 12 + termLengthMonths;
+		numPayments = totalMonths / installementPeriodMonths;
 	}
 
+
+	public double getPayment() {
+		return payment;
+	}
+
+	private void calcAdjustedMarketRate() {
+		adjustedMarketRate = (marketRate / 100.000) * (installementPeriodMonths / 12.00);
+	}
+	
+	private void calcAdjustedCouponRate() {
+		adjustedCouponRate = (couponRate / 100.000) * (installementPeriodMonths / 12.00);		
+	}
+
+
+	public double getAdjustedIssueAmount() {
+		return adjustedIssueAmount;
+	}
+	
+	public int getNumPayments() {
+		return numPayments;
+	}
 }
+
